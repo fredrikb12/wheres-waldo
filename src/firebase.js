c97a777f97743d8e5b3d8748160b82ad83903fe4
fredrikb12 <fredrik.backman12@gmail.com>
2022-04-03 19:54:25 +0200

    Add firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,12 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  getDoc,
+  doc,
+} from "firebase/firestore/lite";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -15,3 +22,13 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+async function getImageData() {
+  const docRef = doc(db, "image-data", "stage-1");
+  const docSnap = await getDoc(docRef);
+  const data = await docSnap.data();
+  return data;
+}
+
+export { getImageData };
